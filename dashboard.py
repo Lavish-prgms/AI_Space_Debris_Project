024d570bbfb60f74570e8dbc2c5d4e2c187c3895
Lavish <lavish9454@gmail.com>
2026-05-03 22:04:32 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -91,9 +91,9 @@ if uploaded:
     image = Image.open(uploaded).convert("RGB")
 else:
     try:
-        image = Image.open("images/space.jpg").convert("RGB")
+       image = Image.open("images/space.jpg").convert("RGB")
 except:
-       image = Image.new("RGB", (900, 500), (5, 10, 25))
+    image = Image.new("RGB", (900, 500), (5, 10, 25))
 
 img = np.array(image)
 draw = img.copy()
